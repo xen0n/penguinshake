45b7f72ddfc0e53f15959e08445ccee6bf4939e5
WANG Xuerui <git@xen0n.name>
2020-08-02 02:49:38 +0800

    working installation

--- do-compile.sh
+++ do-compile.sh
@@ -16,6 +16,7 @@ compile_config () {
     local config_name="$1"
     local config_path="$my_dir/configs/$config_name.config"
     local env_path="$my_dir/env/$config_name"
+    local dist_path="$my_dir/dist/$config_name.tar.zst"
 
     . "$env_path"
     : "${ARCH:?ARCH must be set}"
@@ -25,17 +26,41 @@ compile_config () {
     : "${JOBS:?JOBS must be set}"
 
     local tmpdir="$(mktemp --tmpdir -d penguinshake.$config_name.XXXXXX)"
+    local install_prefix="$(mktemp --tmpdir -d penguinshakedist.$config_name.XXXXXX)"
+    local install_path="$install_prefix/boot"
 
     echo "Compiling config $config_name (ARCH=$ARCH)"
     echo "SOURCE=$SOURCE"
     echo "TMPDIR=$tmpdir"
+    echo "INSTALL_PREFIX=$install_prefix"
 
     cp "$config_path" "$tmpdir/.config"
 
     pushd "$SOURCE"
         make -j "$JOBS" O="$tmpdir" ARCH="$ARCH" CROSS_COMPILE="$CROSS_COMPILE" CC="$CC" syncconfig
         time make -j "$JOBS" O="$tmpdir" ARCH="$ARCH" CROSS_COMPILE="$CROSS_COMPILE" CC="$CC"
+
+        # assemble dist root
+        prepare_install_prefix "$install_prefix"
+
+        # dist
+        make -j "$JOBS" O="$tmpdir" ARCH="$ARCH" CROSS_COMPILE="$CROSS_COMPILE" CC="$CC" INSTALL_PATH="$install_path" INSTALL_MOD_PATH="$install_prefix" install modules_install
     popd
+
+    # remove cruft
+    rm -f "$install_path/vmlinux-"*
+
+    # package
+    pushd "$install_prefix"
+        tar -c -f "$dist_path" --zstd -v .
+    popd
+}
+
+prepare_install_prefix () {
+    local prefix="$1"
+
+    mkdir -p "$prefix/boot"
+    mkdir -p "$prefix/lib/modules"
 }
 
 
